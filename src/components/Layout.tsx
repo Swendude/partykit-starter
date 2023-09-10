6029da9940aa6ccc314303b113be7e4c01b81e69
Swendude <swenmulderij@gmail.com>
2023-09-10 12:31:56 +0200

    Responsive

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,9 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 const Layout = ({ children }: LayoutProps) => {
   return (
     <main
-      className={`${inter.className} flex flex-col items-center h-screen bg-stone-300 w-screen px-32 py-6`}
+      className={`${inter.className} flex flex-col items-center h-screen bg-stone-300 w-screen py-6`}
     >
-      <section className="border border-black rounded p-10 shadow bg-stone-50 w-full">
+      <section className="border border-black rounded p-5 shadow bg-stone-50 w-full md:max-w-4xl">
         {children}
       </section>
     </main>
