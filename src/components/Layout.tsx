972f4a70ea3dc022fc11dc9a89c678d7183f0be6
Swendude <swenmulderij@gmail.com>
2023-09-10 12:34:30 +0200

    padding on small

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 const Layout = ({ children }: LayoutProps) => {
   return (
     <main
-      className={`${inter.className} flex flex-col items-center h-screen bg-stone-300 w-screen py-6`}
+      className={`${inter.className} flex flex-col items-center h-screen bg-stone-300 w-screen py-6 px-8`}
     >
       <section className="border border-black rounded p-5 shadow bg-stone-50 w-full md:max-w-4xl">
         {children}
